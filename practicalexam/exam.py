51bfc7d9fa2fc3f977e14d388f92173d29bd7159
swapnacv21 <swapnavenu116@gmail.com>
2024-09-30 13:45:46 +0530

    exam practical

---
+++ practicalexam/exam.py
@@ -0,0 +1,16 @@
+'''Question 12: To remove duplicates in a list'''
+# --------------------------------------------------
+l=[1,2,3,4,5,5,6,7]
+a=[]
+for i in l:
+    if i not in a:
+        a.append(i)
+print(a)
+    
+'''Question 14 : Print the following pattern'''
+# -------------------------------------------------
+
+# a=65
+# for i in range(1,5):
+#     for j in range(i):
+#         chr
